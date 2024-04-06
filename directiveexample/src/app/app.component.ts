cedfed97db126b78cdffce30f1a6bb11cc00695f
nirosha <niroshakannan17@gmail.com>
2024-04-06 13:01:39 +0530

    ng for directive table example with latest bootsstrap example

--- directiveexample/src/app/app.component.ts
+++ directiveexample/src/app/app.component.ts
@@ -1,13 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'directiveexample';
+export class AppComponent 
+{
+  
+  title = 'Directive Example';
+
+  students_detail = [
+    {name:"Shasha",subject:"English",marks:"85",result:"Pass"},
+    {name:"Jotha",subject:"English",marks:"75",result:"Pass"},
+    {name:"Srither",subject:"English",marks:"95",result:"Pass"},
+    {name:"Jashmine",subject:"English",marks:"77",result:"Pass"},
+    {name:"Anisha",subject:"English",marks:"65",result:"Pass"}
+  ];
+  
+
 }
