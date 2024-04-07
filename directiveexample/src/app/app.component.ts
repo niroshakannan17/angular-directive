57412b47dc061ae12fc560d7568c734155e751f4
nirosha <niroshakannan17@gmail.com>
2024-04-07 22:03:01 +0530

    ng switch example is shoen

--- directiveexample/src/app/app.component.ts
+++ directiveexample/src/app/app.component.ts
@@ -1,19 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet,CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
+
 export class AppComponent 
 {
   
   title = 'Directive Example';
-
+  num:string ="0";
   students_detail = [
     {name:"Shasha",subject:"English",marks:"85",result:"Pass"},
     {name:"Jotha",subject:"English",marks:"75",result:"Pass"},
